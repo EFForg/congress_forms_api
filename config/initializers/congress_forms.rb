83022d8579c284f9651e6db76e124338e0406fcb
Peter Woo <peterw@eff.org>
2018-10-19 13:32:07 -0700

    Don't manage contact-congress repo in worker container.
    Fixes #1.

--- config/initializers/congress_forms.rb
+++ config/initializers/congress_forms.rb
@@ -1,1 +1,7 @@
 CongressForms.contact_congress_repository = Rails.root.join("contact_congress").to_s
+
+if ENV["CONTACT_CONGRESS_AUTO_UPDATE"].present?
+  value = ENV["CONTACT_CONGRESS_AUTO_UPDATE"]
+  CongressForms.auto_update_contact_congress =
+    ActiveModel::Type::Boolean.new.cast(value)
+end
