2ad7aa30f9370300180d78be5e7e13b96a0d9655
Peter Woo <peterw@eff.org>
2019-02-12 13:14:15 -0800

    Small change to test mode param

--- app/controllers/fills_controller.rb
+++ app/controllers/fills_controller.rb
@@ -12,7 +12,7 @@ class FillsController < ApplicationController
 
     begin
       status =
-        if @form.fill(fields, submit: !params[:test])
+        if @form.fill(fields, submit: params[:test] != "1")
           "success"
         else
           "failure"
